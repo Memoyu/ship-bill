3fe7961812fbaea7e05843dc3b8170fa03d1c130
Memoyu <mmy6076@outlook.com>
2025-02-27 00:27:31 +0800

    ✨ 添加分类管理页

---
+++ src/ship-bill/src/pages/category/components/label/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <view class="p-2 bg-slate-100 flex justify-between rounded-md">
+    <view>{{ label.name }}</view>
+    <wd-icon v-if="!label.base" name="close-outline" size="22px" />
+  </view>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ label?: any }>()
+</script>
--- src/ship-bill/src/pages/category/index.vue
+++ src/ship-bill/src/pages/category/index.vue
@@ -18,20 +18,79 @@
       left-arrow
       @click-left="handleClickLeft"
       :bordered="false"
-    ></wd-navbar>
+    />
+
+    <wd-tabs v-model="type">
+      <wd-tab title="收入">
+        <view class="mt3 grid grid-cols-3 gap-2">
+          <view v-for="l in incomes" :key="l._id">
+            <LabelItem :label="l" />
+          </view>
+
+          <view
+            class="border-dashed border-2 border-indigo-600 rounded-md flex justify-center items-center"
+            @click="handleClickAddIncome"
+          >
+            <wd-icon name="add" size="20px"></wd-icon>
+          </view>
+        </view>
+      </wd-tab>
+
+      <wd-tab title="支出">
+        <view class="mt3 grid grid-cols-3 gap-2">
+          <view v-for="l in expends" :key="l._id">
+            <LabelItem :label="l" />
+          </view>
+
+          <view
+            class="border-dashed border-2 border-indigo-600 rounded-md flex justify-center items-center"
+            @click="handleClickAddExpend"
+          >
+            <wd-icon name="add" size="20px"></wd-icon>
+          </view>
+        </view>
+      </wd-tab>
+    </wd-tabs>
   </view>
 </template>
 
 <script lang="ts" setup>
+import LabelItem from './components/label/index.vue'
+
 defineOptions({
   name: 'Report',
 })
+
+const incomes = [
+  { _id: '111111', name: '磅费', base: true },
+  { _id: '222222', name: '超时费', base: true },
+  { _id: '333333', name: '提空', base: true },
+  { _id: '444444', name: '封柜费', base: true },
+  { _id: '555555', name: '补贴', base: false },
+  { _id: '666666', name: '提重', base: false },
+  { _id: '777777', name: '自定义来', base: false },
+]
+
+const expends = [
+  { _id: '111111', name: '尿素', base: true },
+  { _id: '222222', name: '停车费', base: true },
+  { _id: '333333', name: '换柜费', base: true },
+  { _id: '444444', name: '高速费', base: true },
+  { _id: '555555', name: '验还柜', base: false },
+  { _id: '666666', name: '修柜费', base: false },
+]
+
+const type = ref<string>('0')
 
 onLoad(() => {})
 
 function handleClickLeft() {
   uni.navigateBack()
 }
+
+const handleClickAddIncome = () => {}
+
+const handleClickAddExpend = () => {}
 </script>
 
 <style>
